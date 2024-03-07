f566fba09dadd9fa40ce44766c07f5ebbb8309e8
unknown <aj.pineda00265@student.tsu.edu.ph>
2024-03-07 18:36:48 +0800

    Pineda

---
+++ main.js
@@ -0,0 +1,5 @@
+const themeToggleBtn = document.querySelector('.btn-mode');
+
+themeToggleBtn.addEventListener('fa-solid fa-sun btn-mode', () =>{
+    document.body.classList.toggle('white-theme');
+});
